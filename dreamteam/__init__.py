6a714d6ca35b2bd7ab4d9df285f454d900f2a111
RcomaShow <54180001+RcomaShow@users.noreply.github.com>
2026-08-09 20:34:00 +0200

    Expose DreamTeam 0.5 runtime

--- dreamteam/__init__.py
+++ dreamteam/__init__.py
@@ -1,7 +1,9 @@
-"""DreamTeam 0.4.5 usability and hardened cost-proof orchestration primitives."""
+"""DreamTeam 0.5 measurement-first orchestration primitives."""
 from .config import Profile, RuntimeCapabilities, RuntimeConfig, Topology
+from .measurement import EfficiencyGate, EfficiencyMetrics, savings_ratio
 from .pricing import ExecutionLane, PriceBook, TokenUsage, estimate_cost, resolve_model
 from .routing import Criticality, Route, RouteDecision, RouteRequest, TaskKind, choose_route
+from .routing_v05 import V05RouteDecision, V05RoutingPolicy, choose_route_v05
 
 __all__ = [
     "RuntimeCapabilities",
@@ -13,11 +15,17 @@ __all__ = [
     "TokenUsage",
     "estimate_cost",
     "resolve_model",
+    "EfficiencyGate",
+    "EfficiencyMetrics",
+    "savings_ratio",
     "Criticality",
     "TaskKind",
     "Route",
     "RouteDecision",
     "RouteRequest",
     "choose_route",
+    "V05RouteDecision",
+    "V05RoutingPolicy",
+    "choose_route_v05",
 ]
-__version__ = "0.4.5"
+__version__ = "0.5.0"
